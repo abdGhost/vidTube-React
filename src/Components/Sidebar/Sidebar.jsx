fe00007daa0a0184a4f756c3ce32deba45111312
abdGhost <ktaher642@gmail.com>
2024-11-08 15:22:24 +0530

    added feed component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -20,47 +20,47 @@ const Sidebar = ({ sidebar }) => {
   return (
     <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
       <div className="shortcut-links">
-        <div className="side-links">
+        <div className="side-link">
           <img src={home} alt="" />
           <p>Home</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={game_icon} alt="" />
           <p>Gaming</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={automobiles} alt="" />
           <p>Automobiles</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={sports} alt="" />
           <p>Sports</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={entertaiments} alt="" />
           <p>Entertaiment</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={tech} alt="" />
           <p>Technology</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={music} alt="" />
           <p>Music</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={blogs} alt="" />
           <p>Blogs</p>
         </div>
 
-        <div className="side-links">
+        <div className="side-link">
           <img src={news} alt="" />
           <p>News</p>
         </div>
@@ -70,23 +70,23 @@ const Sidebar = ({ sidebar }) => {
       {/* Subscribed List */}
       <div className="subscribed-list">
         <h3>Subscribed</h3>
-        <div className="side-links">
+        <div className="side-link">
           <img src={jack} alt="" />
           <p>PewDiePie</p>
         </div>
-        <div className="side-links">
+        <div className="side-link">
           <img src={simon} alt="" />
           <p>MrBeast</p>
         </div>
-        <div className="side-links">
+        <div className="side-link">
           <img src={tom} alt="" />
           <p>Justin Bieber</p>
         </div>
-        <div className="side-links">
+        <div className="side-link">
           <img src={megan} alt="" />
           <p>5-Minute Craft</p>
         </div>
-        <div className="side-links">
+        <div className="side-link">
           <img src={cameron} alt="" />
           <p>Nas Daily</p>
         </div>
